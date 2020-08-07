3ece8851976b4b941f570c22dcb41ee27779ae01
Pete <petelooney81@gmail.com>
2020-08-07 10:09:04 -0500

    changingDayName

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,4 +4,10 @@ let rightNow = new Date();
 // getDate() returns the day of the month from 1-31
 let currentDayOfMonth = rightNow.getDate();
 
-//
+// getDay() returns the current day of the week from 0-6
+// 0 being Sunday and 6 being Saturday.
+// This is used as an index # for the elements in the 
+// dayNames array. 
+let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+"Thursday", "Friday", "Saturday"];
+let dayOfWeek = dayNames[rightNow.getDay()];
