f8018e3d1510be87810205b2e4efb0deb34fabc6
Pete <petelooney81@gmail.com>
2020-08-10 12:07:38 -0500

    challengeDayCalc

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@
 let rightNow = new Date();
 
 // getDate() returns the day of the month from 1-31
-let currentDayOfMonth = rightNow.getDate();
+let dayOfMonth = rightNow.getDate();
 
 // getDay() returns the current day of the week from 0-6
 // 0 being Sunday and 6 being Saturday.
@@ -18,3 +18,9 @@ let monthNames = ["January", "February", "March", "April", "May",
 "June", "July", "August", "September", "October", "November", "December"];
 let monthOfYear = monthNames[rightNow.getMonth()];
 
+// calculates the amount of days left in the challenge.
+// will eventually be a user input for the challenge end date
+// for now it is just hard entered.
+let challengeEndDate = new Date("August 31, 2020 23:59:59");
+let challengeDayDiff = Math.floor((challengeEndDate.getTime() - rightNow.getTime()) / (1000 * 60 * 60 * 24));
+alert(challengeDayDiff);
